261d645950de237146491303c94828875f6e0e0f
Henning 'haenno' Beier <haenno@web.de>
2023-11-18 21:25:11 +0100

    make data and log presistant

--- api/views.py
+++ api/views.py
@@ -3,12 +3,26 @@ import json
 
 from django.http import HttpResponse
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    filename="./data/api.log",
+)
 
 
 def tick(request):
+    # start processing the request with write to log
+    logging.debug(request.headers)
+    logging.debug(f"Path requested: '{request.path}'")
+
+    # do the actual processing here
+    pass
+
+    # finish up, prepare response
     message = "tock"
     data = {"status": 200, "message": message}
-    logging.info(data)
+
+    # close processing the request with another entry to log
+    logging.debug(f"Now sending response: '{data}'")
     data = json.dumps(data)
     return HttpResponse(data, content_type="application/json", status=200)
